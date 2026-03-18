f3a3f0bbb209e692b6aeee91fb051bd698ed511b
Saloni <saloni.p@idsil.com>
2026-03-18 17:04:06 +0530

    added payment api

--- academy/payments/views.py
+++ academy/payments/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from .models import Payment
+from .serializer import PaymentSerializer
 
-# Create your views here.
+class PaymentViewSet(ModelViewSet):
+    serializer_class = PaymentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Payment.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user, status='success')
