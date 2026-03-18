3e33ff564de1bb5879c1a3f78f4702016e9f2f5c
Saloni <saloni.p@idsil.com>
2026-03-18 16:58:52 +0530

    added payment model

--- academy/payments/models.py
+++ academy/payments/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from courses.models import Course
 
-# Create your models here.
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ('pending', 'Pending'),
+            ('success', 'Success'),
+            ('failed', 'Failed')
+        ],
+        default='pending'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.course} - {self.status}"
