235fdbf7788ede13895906efec1ed97d2841b756
Saloni <saloni.p@idsil.com>
2026-03-19 11:19:52 +0530

    added payment validation before enrollment

--- academy/enrollments/views.py
+++ academy/enrollments/views.py
@@ -3,6 +3,8 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from .models import Enrollment
 from .serializer import EnrollmentSerializer
+from payments.models import Payment
+from rest_framework.exceptions import ValidationError
 
 
 class EnrollmentViewSet(ModelViewSet):
@@ -14,4 +16,16 @@ class EnrollmentViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     
     def perform_create(self, serializer):
-      serializer.save(student=self.request.user)
+       user = self.request.user
+       course = serializer.validated_data.get("course")
+
+       payment_exists = Payment.objects.filter(
+           user=user,
+           course=course,
+           status='success'
+           ).exists()
+
+       if not payment_exists:
+        raise ValidationError("You must complete payment before enrolling.")
+
+       serializer.save(student=user)
--- academy/payments/views.py
+++ academy/payments/views.py
@@ -5,6 +5,7 @@ from .models import Payment
 from .serializer import PaymentSerializer
 
 class PaymentViewSet(ModelViewSet):
+    queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
     permission_classes = [IsAuthenticated]
 
